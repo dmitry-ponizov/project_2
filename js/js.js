24ad6bdf627e7783e47377f19edafa9ae8c3e663
Dmitry Ponizov <dxy@mail.ru>
2016-11-16 11:06:24 +0200

    new

---
+++ js/js.js
@@ -0,0 +1,35 @@
+$('.sl').slick({
+    autoplay:true,
+	autoplaySpeed: 4000,
+    speed:1000,
+	arrows: false,
+	dots:true,
+	pauseOnHover: false,
+    vertical:true,
+});
+$('.sl2').slick({
+    autoplay:true,
+	autoplaySpeed: 4000,
+    speed:1000,
+	arrows: false,
+	dots:true,
+	pauseOnHover: false,
+    vertical:true,
+});
+$('.sl3').slick({
+    autoplay:true,
+	autoplaySpeed: 4000,
+    speed:1000,
+	arrows: false,
+	dots:true,
+	pauseOnHover: false,
+});
+$('.sl4').slick({
+    autoplay:true,
+	autoplaySpeed: 4000,
+    speed:1000,
+	arrows: false,
+	dots:true,
+	pauseOnHover: false,
+});
+
