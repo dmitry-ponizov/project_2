f1b36664f1899abc54e2ace851d4557b0952142c
Dmitry Ponizov <dxy@mail.ru>
2016-11-22 17:19:55 +0200

    new

--- js/js.js
+++ js/js.js
@@ -1,5 +1,5 @@
 $('.sl').slick({
-    autoplay:true,
+//    autoplay:true,
 	autoplaySpeed: 4000,
     speed:1000,
 	arrows: false,
@@ -8,7 +8,7 @@ $('.sl').slick({
     vertical:true,
 });
 $('.sl2').slick({
-    autoplay:true,
+//    autoplay:true,
 	autoplaySpeed: 4000,
     speed:1000,
 	arrows: false,
@@ -17,7 +17,7 @@ $('.sl2').slick({
     vertical:true,
 });
 $('.sl3').slick({
-    autoplay:true,
+//    autoplay:true,
 	autoplaySpeed: 4000,
     speed:1000,
 	arrows: false,
@@ -25,11 +25,20 @@ $('.sl3').slick({
 	pauseOnHover: false,
 });
 $('.sl4').slick({
-    autoplay:true,
+//    autoplay:true,
 	autoplaySpeed: 4000,
     speed:1000,
 	arrows: false,
 	dots:true,
 	pauseOnHover: false,
 });
+ $(function(){
+    $('.circlestat').circliful();
+ });
 
+$(".button").on("click",function(){
+  $(".nav-bar__menu").toggle(1000);
+  });
+$(".main-content__button_a2").on("click",function(){
+  $(".nav-bar__menu").slideToggle(1000);
+  });
